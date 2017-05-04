fe766b52b7b912f25a0370792fca5bf73567fb86
khwajaskm <khwaja.sheik@hitachiconsulting.com>
2017-05-04 15:38:51 +0530

    changed file

--- HelloWorld_App/Program.cs
+++ HelloWorld_App/Program.cs
@@ -11,6 +11,8 @@ namespace HelloWorld_App
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            Console.WriteLine("Hello hi");
+
         }
     }
 }
